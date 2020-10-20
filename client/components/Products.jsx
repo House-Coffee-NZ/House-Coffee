f884735b1114c92e99cdb172cc79dfefbabfcfef
matthew-devonport <roarsintheeast@gmail.com>
2020-10-21 11:16:48 +1300

    created Products component

---
+++ client/components/Products.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Header from './Header'
+import Footer from './Footer'
+
+class Products extends React.Component {
+
+
+    render() {
+        return (
+            <React.Fragment>
+                <div>
+                    <Header />
+                    <Footer />
+                </div>
+            </React.Fragment>
+
+        )
+    }
+}
+
+
+export default Products
