d454e50cae65fa2b803a769b730478cace4bae38
matthew-devonport <roarsintheeast@gmail.com>
2020-11-05 19:41:17 +1300

    fixed spelling mistake

--- client/components/Main.jsx
+++ client/components/Main.jsx
@@ -38,7 +38,7 @@ class Main extends React.Component {
                         <img src="./img/domobar.jpeg" alt="Domobar Logo" className="brandPic">
                         </img>
                         </a>    
-                        <a href="https://domobar.co.nz/espresso-machines/domobar-super-espresso-machine/overview" target="_blank" className="learnButton domoButton">DOMORBAR ESPRESSO MACHINES</a>
+                        <a href="https://domobar.co.nz/espresso-machines/domobar-super-espresso-machine/overview" target="_blank" className="learnButton domoButton">DOMOBAR ESPRESSO MACHINES</a>
                         </div>
                     </div>
                 </div>
